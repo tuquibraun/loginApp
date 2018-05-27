1070c79b74a3f0723f719dfdf984725f1789b5bd
Tuqui Braun <tuquibraun@gmail.com>
2018-05-27 16:46:00 -0300

    autenticacion y registro de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,12 @@ import { SingInComponent } from './sing-in/sing-in.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +29,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/sing-in/sing-in.component.ts
+++ src/app/sing-in/sing-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sing-in',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sing-in.component.scss']
 })
 export class SingInComponent implements OnInit {
+  public email: string;
+  public password: string;
 
-  constructor() { }
-
+  constructor(
+    public authService: AuthService,
+    public router: Router
+  ) {}
   ngOnInit() {
+  }
+  onSubmitAddUser(){
+    this.authService.registerUser(this.email, this.password)
+    .then( (res) =>{
+      this.router.navigate(['/welcome'])
+    }).catch( (err) =>{
+      console.log(err);
+    });
   }
 
 }
