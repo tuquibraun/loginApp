970dc48a1f2ebbadad83b5a4adf19196f7972372
Tuqui Braun <tuquibraun@gmail.com>
2018-05-26 21:11:55 -0300

    navegacion y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'log-in', component: LogInComponent},
+  {path: 'sing-in', component: SingInComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
