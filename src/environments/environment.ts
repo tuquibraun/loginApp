b464513ccac2208f15d44e0c46d92219b2aca504
Tuqui Braun <tuquibraun@gmail.com>
2018-05-27 00:23:45 -0300

    algo de html

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDIfO67bz-yq77OdhlQjaFI_LJpI3yANd0",
+    authDomain: "loginapp-d8c6d.firebaseapp.com",
+    databaseURL: "https://loginapp-d8c6d.firebaseio.com",
+    projectId: "loginapp-d8c6d",
+    storageBucket: "loginapp-d8c6d.appspot.com",
+    messagingSenderId: "296510852260"
+  }
 };
 
 /*
